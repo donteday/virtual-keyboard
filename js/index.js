12cdebc365d786955d127161df2889452b9b60fe
donteday <meowskyj@yandex.ru>
2023-05-01 16:50:28 +0300

    fix: fix switch language v3

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ document.onkeydown = function(event) {
         else pressedKeys.push('Shift');
     }
 
-    if (pressedKeys.toString() === 'Alt,Ctrl') {
+    if (pressedKeys.toString() === 'Alt,Shift') {
         keyboard.pressKey('Lang');
         pressedKeys = [];
         body.children[3].remove();
